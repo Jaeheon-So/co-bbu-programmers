3847a0c53ebd61de8f0019d4fdf3252658dce984
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-06-16 20:44:47 +0900

    오타 수정

--- 1단계/행렬의 덧셈/hyunsoo.js
+++ 1단계/행렬의 덧셈/hyunsoo.js
@@ -3,7 +3,7 @@ function solution(arr1, arr2) {
   
   for(let i = 0; i < arr1.length; i+=1){
       let sum = [];
-      for(let j = 0; j<arr1[i].length; j+=1){;
+      for(let j = 0; j<arr1[i].length; j+=1){
           sum.push(arr1[i][j] + arr2[i][j]);
       }
       answer.push(sum);
