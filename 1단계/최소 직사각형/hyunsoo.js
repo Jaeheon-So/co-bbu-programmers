d0d8bcd7e8644749e68d33bb8b23bef6de6d3ee1
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-06-28 02:01:31 +0900

    1단계 크기가 작은 부분 문자열

--- 1단계/최소 직사각형/hyunsoo.js
+++ 1단계/최소 직사각형/hyunsoo.js
@@ -1,16 +1,10 @@
 function solution(sizes) {
   var answer = 0;
-  let max = [0, 0];
+  let max = [0, 0]
   for(let i = 0; i < sizes.length; i+=1){
       sizes[i].sort((a, b) => a - b);
-      if(max[0] < sizes[i][0]) {
-          max.shift();
-          max.unshift(sizes[i][0]);
-      }
-      if(max[1] < sizes[i][1]) {
-          max.pop();
-          max.push(sizes[i][1]);
-      }
+      if(max[0] < sizes[i][0]) max.splice(0, 1, sizes[i][0]);
+      if(max[1] < sizes[i][1]) max.splice(1, 1, sizes[i][1]);
   }
   return answer = max[0] * max[1];
 }
