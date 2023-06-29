ea4526427e867c2d9f4ab612431a6bbee61eec61
quokka-eating-carrots <ovzip7@gmail.com>
2023-06-29 18:21:08 +0900

    민정 1단계

---
+++ 1단계/문자열 내 마음대로 정렬하기/minjeong.js
@@ -0,0 +1,9 @@
+function solution(strings, n) {
+  strings.sort((a, b) => {
+    if (a[n] < b[n]) return -1;
+    if (a[n] === b[n]) return a < b ? -1 : 1;
+    if (a[n] > b[n]) return 1;
+    return 0;
+  });
+  return strings;
+}
