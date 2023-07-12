1ba4f8dbb656543a8fd8bdeb6e36c8a80b9869e1
quokka-eating-carrots <ovzip7@gmail.com>
2023-07-12 21:20:24 +0900

    민정 1단계 소수 만들기

---
+++ 1단계/소수 만들기/minjeong.js
@@ -0,0 +1,21 @@
+let sieve = new Array(3001).fill(true);
+for (let i = 2; i * i < sieve.length; i += 1) {
+  if (!sieve[i]) continue;
+  for (let j = i + i; j < sieve.length; j += i) {
+    sieve[j] = false;
+  }
+}
+function solution(nums) {
+  var answer = 0;
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
+      for (let k = j + 1; k < nums.length; k += 1) {
+        const index = nums[i] + nums[j] + nums[k];
+        if (sieve[index]) {
+          answer += 1;
+        }
+      }
+    }
+  }
+  return answer;
+}
