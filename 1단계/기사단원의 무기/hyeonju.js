4b5b05e7f1f73747f9733b93e3200ba101fff7f0
iziz9 <46iz.us2@gmail.com>
2023-07-20 20:25:54 +0900

    1단계/기사단원의 무기

--- 1단계/기사단원의 무기/hyeonju.js
+++ 1단계/기사단원의 무기/hyeonju.js
@@ -14,3 +14,20 @@ function solution(number, limit, power) {
 }
 
 //통과
+function solution(number, limit, power) {
+  let divisorArr = Array(number).fill(0)
+  let answer = 0;
+  for (let i = 1; i <= number; i += 1) {
+    for (let j = 1; j <= Math.sqrt(i); j += 1) {
+      if (i % j === 0) {
+        if (j === Math.sqrt(i)) {
+          divisorArr[i - 1] += 1
+        } else {
+          divisorArr[i - 1] += 2
+        }
+      }
+    }
+    answer += divisorArr[i - 1] <= limit ? divisorArr[i - 1] : power
+  }
+  return answer;
+}
