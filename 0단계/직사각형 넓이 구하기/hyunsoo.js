b3895a5a026c0932529da58815ccc0321b2bbf72
HyunSooBae <hyunsoobae.dev@gmail.com>
2023-09-05 16:04:14 +0900

    0단계 직사각형 넓이 구하기

---
+++ 0단계/직사각형 넓이 구하기/hyunsoo.js
@@ -0,0 +1,9 @@
+function solution(dots) {
+  var answer = 0;
+  let arrX = dots.map(el => el[0])
+  let arrY = dots.map(el => el[1])
+  
+  answer = (Math.max(...arrX) - Math.min(...arrX)) * (Math.max(...arrY) - Math.min(...arrY))
+  
+  return answer;
+}
